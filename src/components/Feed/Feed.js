6ae7b4ebd284d2eec50ae427bd12eb531ccaa41c
Moin Khan <nurmoin05@gmail.com>
2022-05-07 19:38:51 +0600

    search area completed

--- src/components/Feed/Feed.js
+++ src/components/Feed/Feed.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import CreateIcon from '@mui/icons-material/Create';
+import ImageIcon from '@mui/icons-material/Image';
+import SubscriptionIcon from '@mui/icons-material/Subscriptions';
+import EventNoteIcon from '@mui/icons-material/EventNote';
+import CalendarViewDayIcon from '@mui/icons-material/CalendarViewDay';
 import './Feed.css';
+import InputOpntion from '../InputOption/InputOpntion';
 
 const Feed = () => {
     return (
@@ -9,11 +14,19 @@ const Feed = () => {
                 <div className="feed__input">
                 <CreateIcon />
                 <form>
-                    <input type="text" />
+                    <input type="text" placeholder="Start a Post" />
                     <button type="submit">Send</button>
                 </form>
                 </div>
+                <div className="feed__inputOption">
+                    <InputOpntion Icon={ImageIcon} title='Photo' color="#70B5FC" />
+                    <InputOpntion Icon={SubscriptionIcon} title='Video' color="#E7A33E" />
+                    <InputOpntion Icon={EventNoteIcon} title='Event' color="#C0CbCd" />
+                    <InputOpntion Icon={CalendarViewDayIcon} title='Write article' color="#7FC15E" />
+                </div>
             </div>
+
+            {/*  */}
         </div>
     );
 };
